d09d447710564ed550f06c276855802a3e37586b
Richard Benny <richardvictorbenny@gmail.com>
2024-02-23 09:17:45 +0000

    personal details page almost done

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,21 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+
+
 // Add your routes here
+router.post('/personal-details-answer', function(req, res) {
+    var givenName = req.session.data['given-name']
+    var surName = req.session.data['sur-name']
+    var dateOfbirth = req.session.data['date-of-birth']
+
+    console.log(dateOfbirth)
+    console.log(givenName)
+    console.log(surName)
+
+    if(givenName === "" && surName === ""){
+        res.redirect('/ineligible-personal-details')
+    } else (
+        res.redirect('/')
+    )
+})
